0dd1e0474f4419332658b406a6d3ae283777f12c
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2022-08-13 08:54:01 +0900

    update

--- 백준/boj_12865_평범한배탕.java
+++ 백준/boj_12865_평범한배탕.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class boj_12865_평범한배탕 {
 
     public static void main(String[] args) throws IOException {
-        int N, K;
+        int N, K, ans;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,19 +25,24 @@ public class boj_12865_평범한배탕 {
 
 
         ans = solve(W,V,N,K);
+
+        System.out.println(ans);
     }
 
     public static int solve(int[] W,int[] V, int N,int K){
 
-        int[][] data = new int[N][K+1];
-        for(int i = 1; i<=K;i++){
-            for (int j = 1; j<=N;j++){
-                
+        int[][] data = new int[N+1][K+1];
+        for(int k = 1; k<=K;k++){
+            for (int i = 1; i<=N;i++){
+                data[i][k] = data[i-1][k];
+                if(k >= W[i]){
+                    data[i][k] = Math.max(data[i][k], data[i-1][k-W[i]] + V[i]);
+                }
             }
         }
 
 
-        return data[N-1][K];
+        return data[N][K];
     }
 
 }
