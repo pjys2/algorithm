9cc1628c715c390987351f8947287a2a3ac21c3d
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2022-08-25 23:09:09 +0900

    update

--- 백준/boj_15685_드래곤커브.java
+++ 백준/boj_15685_드래곤커브.java
@@ -1,10 +1,21 @@
 package 백준;
 
-public class boj_15685_드래곤커브 {
+import java.io.*;
+
+public class boj_15685_드래곤커브{
 
     public static int N, x,y,d,g;
-    
-    public static void main(String[] args) {
+    public static int[] dr = {0,-1,0,1};
+    public static int[] dc = {1,0,-1,0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+
+        for(int i = 0 ; i < N; i++){
+
+        }
+
 
     }
 }
