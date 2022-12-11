c179a1de3983a8795b891269dfadec304f250876
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2022-12-11 22:54:55 +0900

    update

--- 백준/boj_17837_새로운게임2__v2.java
+++ 백준/boj_17837_새로운게임2__v2.java
@@ -7,8 +7,32 @@ import java.util.StringTokenizer;
 
 public class boj_17837_새로운게임2__v2 {
 
+    public static int N, K;
+    public static int[][] map;
 
-    public static void main(String[] args) {
+    public static class Horse{
+        int r,c,dir;
 
+        public Horse(int r, int c, int dir){
+            this.r = r;
+            this.c = c;
+            this.dir = dir;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+
+        for(int r = 0; r<N;r++){
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c<N;c++){
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
     }
 }
