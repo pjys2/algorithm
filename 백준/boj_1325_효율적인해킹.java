6ec3589b450c4026fa638fe872fb6eb4910143b9
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2022-12-24 14:21:00 +0900

    update

--- 백준/boj_1325_효율적인해킹.java
+++ 백준/boj_1325_효율적인해킹.java
@@ -5,80 +5,76 @@ import com.sun.xml.internal.bind.v2.model.core.EnumLeafInfo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class boj_1325_효율적인해킹 {
-    public static int N,M;
+    public static int N, M;
+
+    public static int[][] pc;
+    public static int count;
+    public static int[] counts;
 
     public static class Node{
         int num;
         Node node;
-
         public Node(int num, Node node){
-            this.num = num;
+            this.num= num;
             this.node = node;
         }
     }
-
-    public static Node[] nodeList;
-    public static int[] counts;
-    public static int count;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        nodeList = new Node[N+1];
+        pc = new int[N+1][N+1];
         counts = new int[N+1];
+
         for (int i = 0; i<M;i++){
             st = new StringTokenizer(br.readLine());
             int to = Integer.parseInt(st.nextToken());
             int from = Integer.parseInt(st.nextToken());
 
-            nodeList[from] = new Node(to, nodeList[from]);
+            pc[from][to] = 1;
         }
+
 
         int max = 0;
-
-        for (int i = 1; i<=N;i++){
+        for (int r = 1; r<= N;r++){
             count = 0;
-            DFS(i);
-            counts[i] = count;
+            DFS(r);
+            counts[r] = count;
 
-            if(max < counts[i]){
-                max = counts[i];
+            if(max < counts[r]){
+                max = counts[r];
             }
         }
 
-        List<Integer> ansList = new ArrayList<>();
-        for (int i = 1; i<=N;i++){
-            if(counts[i] == max){
-                ansList.add(i);
+
+        StringBuilder sb = new StringBuilder();
+        for (int r = 1; r<= N;r++){
+            if(counts[r] == max){
+                sb.append(r+" ");
             }
         }
 
-        for (int i : ansList){
-            System.out.print(i + " ");
-        }
+        System.out.println(sb);
 
     }
 
-    public static void DFS(int i){
-        count++;
 
-        for (Node temp = nodeList[i]; temp != null; temp = temp.node){
-            if(counts[temp.num] != 0){
-                count += counts[temp.num];
-                continue;
+    public static void DFS(int r){
+        for (int c = 1; c<=N;c++){
+            if (pc[r][c] == 1){
+                count++;
+                DFS(c);
             }
-
-            DFS(temp.num);
         }
     }
 }
