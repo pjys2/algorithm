2dea046315f14f4d659932195be550b418df3b33
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2022-12-31 00:42:39 +0900

    update

---
+++ 백준/boj_2251_물통.java
@@ -0,0 +1,20 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_2251_물통 {
+    public static int A,B,C;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        
+    }
+}
