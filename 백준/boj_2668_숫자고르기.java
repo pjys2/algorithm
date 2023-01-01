c2045470b639f0bb7782266935d877993ac41bb2
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-01 20:54:50 +0900

    update

--- 백준/boj_2668_숫자고르기.java
+++ 백준/boj_2668_숫자고르기.java
@@ -5,15 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class boj_2251_물통 {
-    public static int A,B,C;
+public class boj_2668_숫자고르기 {
+
+    public static int N;
+    public static int[][] chart;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+
+        chart = new int[2][N+1];
+
+        for (int i = 1; i<N;i++){
+            int num = Integer.parseInt(br.readLine());
+            
+        }
+
 
 
     }
