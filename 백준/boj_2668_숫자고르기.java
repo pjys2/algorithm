5bfa11f1d5d1901315d9c2559d9dbed16129ef95
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-02 18:19:45 +0900

    update

--- 백준/boj_2668_숫자고르기.java
+++ 백준/boj_2668_숫자고르기.java
@@ -11,25 +11,59 @@ import java.util.StringTokenizer;
 public class boj_2668_숫자고르기 {
 
     public static int N,count;
+    public static boolean[] visited;
+    public static int[][] selectSet;
+    public static int[][] select;
     public static int[] numList;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         numList = new int[N+1];
-
+        selectSet = new int[2][N+1];
+        boolean[] visitSet = new boolean[N+1];
+        int countSet = 0;
         for (int i = 1; i <= N; i++){
-            
+            int number = Integer.parseInt(br.readLine());
+            numList[i] = number;
+
+            if(number == i){
+                countSet++;
+                visitSet[i] = true;
+                select[0][i] = number;
+                select[1][i] = number;
+            }
+
+        }
+
+        for (int i = 1; i<=N;i++){
+            visited = visitSet;
+            count = countSet;
+            select = selectSet;
+            DFS(i);
         }
 
     }
 
 
     public static void DFS(int i){
+        count++;
+        if(!visited[numList[i]]){
+            DFS(i);
+        }
 
     }
 
+    public static boolean IsEqual(){
 
+        for (int i = 1; i <= N;i++){
+            if(select[0][i] != select[1][i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     public static void print(){
         for (int i = 1; i<=N;i++){
