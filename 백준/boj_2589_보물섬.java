bcf8064c5a5db12de32c533d3eaec8debb28c05f
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-05 17:31:05 +0900

    update

--- 백준/boj_2589_보물섬.java
+++ 백준/boj_2589_보물섬.java
@@ -3,18 +3,33 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj_2589_보물섬 {
-    public static int R,C;
+    public static int R,C,ans;
     public static char[][] map;
+    public static class Point{
+        int r, c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+    }
+
+    public static int[] dr = {1,-1,0,0};
+    public static int[] dc = {0,0,1,-1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-
+        ans = 0;
         map = new char[R][C];
 
         for (int r = 0; r<R;r++){
@@ -24,7 +39,42 @@ public class boj_2589_보물섬 {
             }
         }
 
-        print();
+        for (int r = 0; r<R;r++){
+            for (int c = 0; c<C;c++){
+                BFS(new Point(r,c));
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+
+    public static void BFS(Point start){
+        Queue<Point> queue = new LinkedList<Point>();
+        queue.offer(start);
+        boolean[][] visited = new boolean[R][C];
+        visited[start.r][start.c] = true;
+        int length = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int s = 0; s < size;s++){
+                Point current = queue.poll();
+                for (int d = 0; d<4;d++){
+                    int nr = current.r+dr[d];
+                    int nc = current.c+dc[d];
+
+                    if(nr >= 0 && nr < R && nc >= 0 && nc < C && !visited[nr][nc]&& map[nr][nc] =='L'){
+                        queue.offer(new Point(nr,nc));
+                        visited[nr][nc] = true;
+                    }
+                }
+            }
+            if(length > ans){
+                ans = length;
+            }
+            length++;
+        }
 
     }
 
