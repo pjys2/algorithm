b716796b02f72167ca20514c025eda513ed0e991
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-08 22:58:04 +0900

    update

--- 백준/boj_2210_숫자판점프.java
+++ 백준/boj_2210_숫자판점프.java
@@ -1,12 +1,24 @@
 package 백준;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj_2210_숫자판점프 {
 
-
-    public static void main(String[] args) {
+    public static int[] dr = {0,0,1,-1};
+    public static int[] dc = {1,-1,0,0};
+    public static int[][] board;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        board = new int[5][5];
+
+        for (int i = 0; i<5;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0;j<5;j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
     }
 }
