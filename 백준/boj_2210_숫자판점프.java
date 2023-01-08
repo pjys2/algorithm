f46b212aaae53c9129f75a2a4cde419a97edfd11
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-08 22:54:18 +0900

    update

---
+++ 백준/boj_2210_숫자판점프.java
@@ -0,0 +1,12 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj_2210_숫자판점프 {
+
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
