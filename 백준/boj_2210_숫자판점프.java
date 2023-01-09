f6a65495b0001481edaef74e6a0743973c646d7f
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-09 21:03:01 +0900

    update

--- 백준/boj_2210_숫자판점프.java
+++ 백준/boj_2210_숫자판점프.java
@@ -3,6 +3,8 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class boj_2210_숫자판점프 {
@@ -10,6 +12,17 @@ public class boj_2210_숫자판점프 {
     public static int[] dr = {0,0,1,-1};
     public static int[] dc = {1,-1,0,0};
     public static int[][] board;
+
+    public static class Point{
+        int r, c;
+
+        public Point(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         board = new int[5][5];
@@ -18,6 +31,27 @@ public class boj_2210_숫자판점프 {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0;j<5;j++){
                 board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        Map<Integer,Integer> numList = new HashMap<>();
+
+        for (int r = 0; r<5;r++){
+            for (int c = 0; c<5;c++){
+                DFS(new Point(r,c), 0);
+            }
+        }
+    }
+
+    public static void DFS(Point point, int cnt){
+
+
+        for (int d = 0; d<4;d++){
+            int nr = point.r+dr[d];
+            int nc = point.c+dc[d];
+            if(nr >= 0 && nr <5 && nc >= 0 && nc <5){
+
             }
         }
     }
