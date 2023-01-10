8f8fc08a6b9983874d240ec4fc202344d91c06ea
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-10 22:05:19 +0900

    update

--- 백준/boj_2668_숫자고르기.java
+++ 백준/boj_2668_숫자고르기.java
@@ -3,99 +3,62 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.StringTokenizer;
+
 
 public class boj_2668_숫자고르기 {
-
-    public static int N,count;
-    public static boolean[] visited;
-    public static boolean[][] select;
+    public static int N, ans;
+    public static List<Integer> ansList;
     public static int[] numList;
-    public static void main(String[] args) throws IOException {
+    public static boolean[] visited;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
         numList = new int[N+1];
-        boolean[][] selectSet = new boolean[2][N+1];
-        boolean[] visitSet = new boolean[N+1];
-        int countSet = 0;
-        boolean[] ansList = new boolean[N+1];
-        for (int i = 1; i <= N; i++){
-            int number = Integer.parseInt(br.readLine());
-            numList[i] = number;
-
-            if(number == i){
-                countSet++;
-                visitSet[i] = true;
-                selectSet[0][i] = true;
-                selectSet[1][number] = true;
-            }
+        visited = new boolean[N+1];
+        ansList = new ArrayList<>();
+        for (int i =1; i<=N;i++){
+            numList[i] = Integer.parseInt(br.readLine());
         }
 
-        for (int i = 0; i<2;i++){
-            for (int j = 0; j < selectSet[i].length;j++){
-                System.out.print(selectSet[i][j]+" ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("----------");
-        int max = 0;
         for (int i = 1; i<=N;i++){
-            visited = visitSet;
-            count = countSet;
-            select = selectSet;
 
-            DFS(i);
+            if(!visited[i]){
 
-            if(IsEqual()){
-                if(count > max){
-                    max = count;
-                    ansList = select[1];
-
-                }
-            }
-
-        }
-        System.out.println(max);
-
-
-
-        for (int i = 0; i<ansList.length;i++){
-            if(ansList[i]){
-                System.out.println(i);
-            }
-        }
-    }
-
-
-    public static void DFS(int i){
-        count++;
-        visited[i] = true;
-        select[0][i] = true;
-        select[1][numList[i]] = true;
-        if(!visited[numList[i]]){
-            DFS(numList[i]);
-        }
-    }
-
-    public static boolean IsEqual(){
-
-        for (int i = 1; i <= N;i++){
-            if(select[0][i] != select[1][i]){
-                return false;
+                visited[i] = true;
+                DFS(i,i);
+                visited[i] = false;
             }
         }
 
-        return true;
-    }
-
-    public static void print(){
-        for (int i = 1; i<=N;i++){
-            System.out.print(numList[i]+" ");
+        System.out.println(ansList.size());
+        Collections.sort(ansList);
+        for (int num : ansList){
+            System.out.println(num);
         }
     }
+
+    public static void DFS(int idx, int target){
+
+        if(numList[idx] == target){
+            ansList.add(target);
+            return;
+        }
+
+        if (!visited[numList[idx]]){
+            visited[numList[idx]] = true;
+            DFS(numList[idx],target);
+            visited[numList[idx]] = false;
+        }
+
+    }
+
+
+
+
+
 }
