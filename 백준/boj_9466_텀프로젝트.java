a71b47d961a46d2f4d548b3baeac2b11e282b998
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-11 15:57:05 +0900

    update

---
+++ 백준/boj_9466_텀프로젝트.java
@@ -0,0 +1,14 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_9466_텀프로젝트 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+    }
+}
