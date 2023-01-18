844170aef5e8d0b42c7486303f50bb031c4c3dfc
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-18 20:15:11 +0900

    update

--- 백준/boj_1325_효율적인해킹.java
+++ 백준/boj_1325_효율적인해킹.java
@@ -5,36 +5,34 @@ import com.sun.xml.internal.bind.v2.model.core.EnumLeafInfo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class boj_1325_효율적인해킹 {
     public static int N, M;
     public static boolean[] visited;
     public static int[] counts;
-    public static Node[] nodeList;
-    public static class Node{
-        int num;
-        Node node;
-        public Node(int num, Node node){
-            this.num= num;
-            this.node = node;
-        }
-    }
+    public static ArrayList<Integer>[] computerList;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        nodeList = new Node[N+1];
+        computerList = new ArrayList[N+1];
         counts = new int[N+1];
+
+        for (int i = 1; i <= M; i++){
+            computerList[i] = new ArrayList<>();
+        }
 
         for (int i = 1; i <= M; i++){
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            nodeList[B] = new Node(A,nodeList[B]);
+            computerList[B].add(A);
         }
+
 
         int max = 0;
 
@@ -47,11 +45,11 @@ public class boj_1325_효율적인해킹 {
                 max = counts[i];
             }
         }
-
         StringBuilder sb = new StringBuilder();
-        for (int r = 1; r<= N;r++){
-            if(counts[r] == max){
-                sb.append(r+" ");
+
+        for (int i = 1; i<=N;i++){
+            if(max == counts[i]){
+                sb.append(i+" ");
             }
         }
 
@@ -61,13 +59,12 @@ public class boj_1325_효율적인해킹 {
 
     public static void DFS(int idx){
 
-
-        for (Node temp = nodeList[idx]; temp!= null; temp = temp.node){
-            if(!visited[temp.num]){
-                counts[temp.num]++;
-                visited[temp.num] = true;
-                DFS(temp.num);
+        for (int num : computerList[idx]){
+            if(!visited[num]){
+                counts[num]++;
+                DFS(num);
             }
         }
+
     }
 }
