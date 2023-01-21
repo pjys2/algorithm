10424eff1944333223a76912ddf594813be3aad9
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-21 21:58:21 +0900

    update

--- 백준/boj_1743_음식물피하기.java
+++ 백준/boj_1743_음식물피하기.java
@@ -3,17 +3,22 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class boj_1743_음식물피하기 {
-    public static int N, M, K, count;
+    public static int N, M, K, ans;
     public static char[][] map;
+    public static boolean[][] visited;
     public static int[] dr = {0,0,1,-1};
     public static int[] dc = {1,-1,0,0};
-    public static int[][] connect;
+    public static class Point{
+        int r, c;
+
+        public Point(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,57 +26,70 @@ public class boj_1743_음식물피하기 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
         map = new char[N+1][M+1];
-        connect = new int[N+1][M+1];
-        for (int k = 1; k <= K; k++){
+        visited = new boolean[N+1][M+1];
+        ans = 0;
+
+        for (int i = 0;i<=N;i++){
+            Arrays.fill(map[i],'.');
+        }
+
+        for (int i = 1; i<=K;i++){
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
+
             map[r][c] = '#';
         }
 
-
-        int number = 1;
-        List<Integer> ansList = new ArrayList<>();
         for (int r = 1; r<=N;r++){
-            count = 0;
             for (int c = 1; c<=M;c++){
-                if(connect[r][c] != 0 || map[r][c] != '#')continue;
-                connect[r][c] = number;
-                DFS(r,c,number);
-
-                number++;
-                ansList.add(count);
-
+                if(!visited[r][c] && map[r][c] =='#') continue;
+                BFS(new Point(r,c));
             }
         }
-        Collections.reverse(ansList);
 
-        System.out.println(ansList.get(0));
+        System.out.println(ans);
+    }
+
+    public static void BFS(Point start){
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(start);
+        boolean[][] visited = new boolean[N+1][M+1];
+        visited[start.r][start.c] = true;
+
+        int size = 1;
+
+
+        System.out.println("출력");
+        while(!queue.isEmpty()){
+            Point current = queue.poll();
+
+            for (int d = 0; d<4;d++){
+                int nr = current.r+dr[d];
+                int nc = current.c+dc[d];
+
+                if(nr >= 1 && nr <=N && nc >= 1 && nc <= M && !visited[nr][nc] && map[nr][nc]=='#'){
+                    size++;
+                    queue.offer(new Point(nr,nc));
+                    visited[nr][nc] = true;
+                    System.out.println(size);
+                }
+            }
+        }
+
+        ans = Math.max(ans,size);
 
     }
 
-    public static void DFS(int r, int c,int number){
-        count++;
-
-        for (int d = 0; d<4;d++){
-            int nr = r+dr[d];
-            int nc = c+dc[d];
-
-            if(nr >= 1 && nr <=N && nc >= 1 && nc <= M && map[nr][nc] == '#' && connect[nr][nc] == 0){
-                connect[nr][nc] = number;
-                DFS(nr,nc, number);
-            }
-        }
-    }
 
     public static void print(){
         for (int r = 1; r<=N;r++){
             for (int c = 1; c<=M;c++){
-                System.out.print(connect[r][c]+" ");
+                System.out.print(map[r][c]+" ");
             }
             System.out.println();
         }
     }
+
 }
