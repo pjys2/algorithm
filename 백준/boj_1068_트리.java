0d36386bdfe53e0e2f13117fc30291759e11855a
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-23 18:52:51 +0900

    update

--- 백준/boj_1068_트리.java
+++ 백준/boj_1068_트리.java
@@ -3,32 +3,54 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class boj_1068_트리 {
-    public static int N;
-    public static int[] node;
+    public static int N, ans;
+    public static ArrayList<Integer>[] nodeList;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        node = new int[N];
+        ans = 0;
+        nodeList = new ArrayList[N];
+
+        for (int i = 0; i<N;i++){
+            nodeList[i] = new ArrayList<>();
+        }
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i<N;i++){
-
-            node[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            if (num == -1) continue;
+            nodeList[num].add(i);
         }
 
         int delNum = Integer.parseInt(br.readLine());
 
-        delete(delNum);
+        nodeList[delNum].clear();
 
 
+        for (int i=0; i<N;i++){
+            int cnt = nodeList[i].size();
+            if(cnt >= 2) continue;
+
+
+            if (cnt == 0){
+                ans++;
+            }else{
+                int child = nodeList[i].get(0);
+
+                if (child == delNum) {
+                    ans++;
+                }
+            }
+        }
+
+        System.out.println(ans);
     }
 
-    public static void delete(int delNum){
-        int nextNum = node[delNum];
-        
-
-    }
 }
