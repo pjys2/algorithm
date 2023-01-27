bba1834575850158a13318d2c652a6c0e0a5fa29
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-27 21:50:38 +0900

    update

--- 백준/boj_24479_알고리즘수업_dfs.java
+++ 백준/boj_24479_알고리즘수업_dfs.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DateFormatSymbols;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class boj_24479_알고리즘수업_dfs {
-    public static int N,M,R;
+    public static int N,M,R,cnt;
     public static int[] ans;
-
+    public static boolean[] visited;
     public static ArrayList<Integer>[] nodeList;
 
     public static void main(String[] args) throws IOException {
@@ -21,6 +22,12 @@ public class boj_24479_알고리즘수업_dfs {
         R = Integer.parseInt(st.nextToken());
         nodeList = new ArrayList[N+1];
         ans = new int[N+1];
+        visited = new boolean[N+1];
+        cnt = 1;
+
+        for (int i = 1; i<=N;i++){
+            nodeList[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i<M;i++){
             st = new StringTokenizer(br.readLine());
@@ -31,12 +38,26 @@ public class boj_24479_알고리즘수업_dfs {
             nodeList[v].add(u);
         }
 
+        for (int i = 1; i<=N;i++){
+            Collections.sort(nodeList[i]);
+        }
+
 
         DFS(R);
+
+        for (int i = 1; i<=N;i++){
+            System.out.println(ans[i]);
+        }
 
     }
 
     public static void DFS(int idx){
-        
+        visited[idx] = true;
+        ans[idx] = cnt++;
+        for (int next : nodeList[idx]){
+            if (!visited[next]){
+                DFS(next);
+            }
+        }
     }
 }
