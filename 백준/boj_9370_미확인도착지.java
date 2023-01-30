83f87aa290e88bab4d8283e333af45d2ddbdf6db
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-30 16:19:19 +0900

    update

--- 백준/boj_9370_미확인도착지.java
+++ 백준/boj_9370_미확인도착지.java
@@ -1,7 +1,28 @@
 package 백준;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class boj_9370_미확인도착지 {
-    public static void main(String[] args) {
-        
+    public static int n, m, t, s,g,h;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            t = Integer.parseInt(st.nextToken());
+
+            s = Integer.parseInt(st.nextToken());
+            g = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+
+
+        }
     }
 }
