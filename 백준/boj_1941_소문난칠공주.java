6fe7662d26a1e9d6fea3a1716f4b0e98df91f99c
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-01-31 22:56:31 +0900

    update

--- 백준/boj_1941_소문난칠공주.java
+++ 백준/boj_1941_소문난칠공주.java
@@ -1,4 +1,33 @@
 package 백준;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class boj_1941_소문난칠공주 {
+
+    public static char[][] table;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        table = new char[5][5];
+
+        for (int r = 0; r<5;r++){
+            String str = br.readLine();
+            for (int c = 0; c<5;c++){
+                table[r][c] = str.charAt(c);
+            }
+        }
+
+        print();
+    }
+
+    public static void print(){
+        for (int r = 0; r<5;r++){
+            for (int c = 0; c<5;c++){
+                System.out.print(table[r][c]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
