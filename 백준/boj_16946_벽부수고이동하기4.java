0cda6ebbdaf521a3117c7faa11eb401d63df90d3
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-05 20:48:35 +0900

    update

--- 백준/boj_16946_벽부수고이동하기4.java
+++ 백준/boj_16946_벽부수고이동하기4.java
@@ -3,6 +3,7 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -10,7 +11,9 @@ import java.util.StringTokenizer;
 public class boj_16946_벽부수고이동하기4 {
     public static int N, M, count;
     public static int[][] map;
+    public static int[][] counts;
     public static int[][] ans;
+    public static boolean[][] visited;
     public static int[] dr = {0,0,1,-1};
     public static int[] dc = {1,-1,0,0};
     public static class Point{
@@ -29,7 +32,9 @@ public class boj_16946_벽부수고이동하기4 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
+        counts = new int[N][M];
         ans = new int[N][M];
+        visited = new boolean[N][M];
 
         for (int r = 0; r<N;r++){
             String str = br.readLine();
@@ -38,17 +43,61 @@ public class boj_16946_벽부수고이동하기4 {
             }
         }
 
-        BFS(0,0);
+        for (int r = 0; r<N;r++){
+            for (int c = 0; c<M;c++){
+                if(map[r][c] != 0 || visited[r][c]) continue;
+
+                BFS(r,c);
+            }
+        }
+
+        for (int r = 0; r<N;r++){
+            for (int c = 0; c<M;c++){
+                if(map[r][c] != 1) continue;
+
+                int sum = 0;
+                for (int d = 0; d<4;d++){
+                    int nr = r+dr[d];
+                    int nc = c+dc[d];
+                    if(nr >= 0 && nr < N && nc >= 0 && nc < M){
+                        sum += counts[nr][nc];
+                    }
+                }
+                sum += 1;
+
+                ans[r][c] = sum;
+            }
+        }
 
         print();
 
     }
+
+    public static int number;
     public static void BFS(int r, int c){
         Queue<Point> queue = new LinkedList<>();
         queue.add(new Point(r,c));
-        boolean[][] visited = new boolean[N][M];
         visited[r][c] = true;
+        ArrayList<Point> pointList = new ArrayList<>();
+        int cnt = 0;
+        while(!queue.isEmpty()){
+            Point current = queue.poll();
 
+            cnt++;
+            pointList.add(new Point(current.r, current.c));
+            for (int d = 0; d<4;d++){
+                int nr = current.r+dr[d];
+                int nc = current.c+dc[d];
+                if(nr >= 0 && nr < N && nc >=0 && nc <M && !visited[nr][nc] && map[nr][nc] == 0){
+                    queue.add(new Point(nr,nc));
+                    visited[nr][nc] = true;
+                }
+            }
+        }
+
+        for (Point point : pointList){
+            counts[point.r][point.c] = cnt;
+        }
 
     }
 
