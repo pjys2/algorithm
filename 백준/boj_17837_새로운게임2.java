c8ceef2566b0f8097c8e9d21ff25873889f79cfc
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-06 17:59:40 +0900

    update

--- 백준/boj_17837_새로운게임2.java
+++ 백준/boj_17837_새로운게임2.java
@@ -3,11 +3,25 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.StringTokenizer;
 
 public class boj_17837_새로운게임2 {
+    public static int N, K;
+    public static int[][] map;
+    public static int[] dr = {};
+    public static int[] dc = {};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) {
-        
+        map = new int[N+1][N+1];
+
+
+
+
+
     }
 }
