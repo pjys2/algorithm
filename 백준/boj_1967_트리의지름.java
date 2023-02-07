13ffa37f0ec8c0af6291ae812b496d35b30c88e7
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-07 13:29:43 +0900

    update

--- 백준/boj_1967_트리의지름.java
+++ 백준/boj_1967_트리의지름.java
@@ -3,6 +3,9 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj_1967_트리의지름 {
@@ -14,37 +17,49 @@ public class boj_1967_트리의지름 {
         int len;
         Node node;
 
-        public Node(int num, Node node){
+        public Node(int num,int len, Node node){
             this.num = num;
+            this.len = len;
             this.node = node;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
 
         nodeList = new Node[N+1];
 
 
         for (int i = 0; i < N-1; i++){
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int len = Integer.parseInt(st.nextToken());
 
-            nodeList[from] = new Node(to,nodeList[from]);
-
+            nodeList[from] = new Node(to,len,nodeList[from]);
+            nodeList[to] = new Node(from,len,nodeList[to]);
         }
 
         ans = 0;
 
-        int first = 0;
-        int second = 0;
-        for (Node temp = nodeList[0]; temp != null; temp = temp.node){
+        for (int i = 1; i<=N;i++){
+            DFS(i, new boolean[N+1],0);
+        }
 
+        System.out.println(ans);
+    }
+
+    public static void DFS(int current, boolean[] visited,int sum) {
+        visited[current] = true;
+
+        for (Node node = nodeList[current]; node != null; node = node.node) {
+            if (!visited[node.num]) {
+                DFS(node.num,visited,sum+node.len);
+                ans = Math.max(ans, sum+node.len);
+            }
         }
     }
+
 }
