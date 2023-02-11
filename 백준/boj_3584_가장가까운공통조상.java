47be2bd2bfe58d87841ec74d61622197a666dc35
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-11 18:19:17 +0900

    update

--- 백준/boj_3584_가장가까운공통조상.java
+++ 백준/boj_3584_가장가까운공통조상.java
@@ -8,8 +8,9 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class boj_3584_가장가까운공통조상 {
-    public static int tc,N,node1,node2;
+    public static int tc,N,node1,node2,ans;
     public static int[] parrent;
+    public static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         tc = Integer.parseInt(br.readLine());
@@ -17,7 +18,7 @@ public class boj_3584_가장가까운공통조상 {
             N = Integer.parseInt(br.readLine());
 
             parrent = new int[N+1];
-
+            visited = new boolean[N+1];
             for (int i = 1; i<N;i++){
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int A = Integer.parseInt(st.nextToken());
@@ -29,40 +30,26 @@ public class boj_3584_가장가까운공통조상 {
             node1 = Integer.parseInt(st.nextToken());
             node2 = Integer.parseInt(st.nextToken());
 
+            ans = 0;
 
-            List<Integer> parrentList1 = new ArrayList<>();
-            List<Integer> parrentList2 = new ArrayList<>();
+            DFS(node1);
+            DFS(node2);
 
-            DFS(node1,parrentList1);
-            DFS(node2,parrentList2);
 
-            int idx1 = parrentList1.size()-1;
-            int idx2 = parrentList2.size()-1;
-
-            int ans = 0;
-            while (true){
-
-                if (idx1 < 0 || idx2 < 0 || parrentList1.get(idx1) != parrentList2.get(idx2)){
-                    ans = parrentList1.get(idx1+1);
-                    break;
-                }
-
-                idx1--;
-                idx2--;
-            }
 
             System.out.println(ans);
 
         }
     }
 
-    public static void DFS(int current, List<Integer> parrentList){
-        if(parrent[current] == 0){
+    public static void DFS(int current){
+        if(visited[current]){
+            ans = current;
             return;
         }
 
-        parrentList.add(current);
+        visited[current] = true;
 
-        DFS(parrent[current],parrentList);
+        DFS(parrent[current]);
     }
 }
