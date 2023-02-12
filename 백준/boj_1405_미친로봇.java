4d6eaabac6a060aa1d2a6bd17e1510b0a7385b62
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-13 00:17:46 +0900

    update

--- 백준/boj_1405_미친로봇.java
+++ 백준/boj_1405_미친로봇.java
@@ -6,11 +6,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class boj_1405_미친로봇 {
-    public static int N;
+    public static int N,e,w,s,n;
+    public static int[] dr = {};
+    public static int[] dc = {};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-    
+        N = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
 
     }
 }
