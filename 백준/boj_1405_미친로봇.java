9e7dd807e1ecff64dc66d637fee58d7c8c57a76b
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-13 00:00:04 +0900

    update

---
+++ 백준/boj_1405_미친로봇.java
@@ -0,0 +1,16 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1405_미친로봇 {
+    public static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+
+    }
+}
