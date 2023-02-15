4e1c525d79bcafce83178b14f223dc892f3f547b
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-15 15:37:58 +0900

    update

---
+++ 백준/boj_16953_A에서B.java
@@ -0,0 +1,19 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_16953_A에서B {
+    public static int A, B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        
+
+    }
+}
