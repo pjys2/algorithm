c56c9f1d262cb2c1503b9fb59b0073516b69ad79
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-15 22:15:51 +0900

    update

--- 백준/boj_1405_미친로봇.java
+++ 백준/boj_1405_미친로봇.java
@@ -3,12 +3,17 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class boj_1405_미친로봇 {
-    public static int N,e,w,s,n;
+    public static int N;
     public static int[] dr = {0,0,1,-1};
     public static int[] dc = {1,-1,0,0};
+    public static boolean[][] visited;
+    public static double[] dirValue;
+    public static double ans;
     public static class Point{
         int r, c;
 
@@ -21,26 +26,39 @@ public class boj_1405_미친로봇 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        w = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
+        ans = 0;
+        dirValue = new double[4];
+        visited = new boolean[30][30];
+        for (int i = 0; i<4;i++){
+            dirValue[i] = Double.parseDouble(st.nextToken())/100;
+        }
+        visited[15][15] = true;
+        DFS(new Point(15,15),0,1);
 
-
-        DFS(new Point(0,0),0);
+        System.out.println(ans);
     }
 
-    public static void DFS(Point current,int k){
+    public static void DFS(Point current,int k,double result){
+
 
         if(k == N){
-            isSimple();
+            ans += result;
             return;
+        }
+
+
+        for (int d = 0; d<4;d++){
+            int nr = current.r+dr[d];
+            int nc = current.c+dc[d];
+
+            if(nr >= 0 && nr <= 30 && nc >= 0 && nc <= 30 && !visited[nr][nc]){
+                visited[current.r][current.c]= true;
+                DFS(new Point(nr,nc),k+1,result*dirValue[d]);
+                visited[current.r][current.c]= false;
+            }
         }
     }
 
-    public static boolean isSimple(){
 
 
-        return true;
-    }
 }
--- 백준/boj_16953_A에서B.java
+++ 백준/boj_16953_A에서B.java
@@ -3,17 +3,65 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class boj_16953_A에서B {
-    public static int A, B;
+    public static long A, B;
+    public static boolean isPossible;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+        if (A <= B){
+            BFS();
+        }
 
-        
+        if(!isPossible){
+            System.out.println(-1);
+        }
+
+    }
+
+    public static void BFS(){
+        Queue<Long> queue = new LinkedList<>();
+        queue.add(A);
+        Set<Long> visitSet = new HashSet<>();
+        visitSet.add(A);
+        int cnt = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for (int s = 1; s<=size;s++){
+                long current = queue.poll();
+
+
+                if(current == B){
+                    System.out.println(cnt+1);
+                    isPossible =true;
+                    return;
+                }
+
+
+                long next = current*2;
+
+                if(next<=B && !visitSet.contains(next)){
+                    queue.add(next);
+                    visitSet.add(next);
+                }
+
+                next = current*10+1;
+
+                if(next<=B && !visitSet.contains(next)){
+                    queue.add(next);
+                    visitSet.add(next);
+                }
+
+            }
+
+            cnt++;
+
+        }
 
     }
 }
