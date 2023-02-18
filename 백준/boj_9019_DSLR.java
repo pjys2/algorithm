ee0fbf71860386e319ba8240d777d66778fd755a
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-18 17:10:28 +0900

    update

---
+++ 백준/boj_9019_DSLR.java
@@ -0,0 +1,11 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj_9019_DSLR {
+    public static int T,A,B;
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
