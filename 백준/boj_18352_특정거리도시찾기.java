4d1526a472d4d99e3e8ef4b0902afdd1760920b5
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-21 16:58:55 +0900

    update

---
+++ 백준/boj_18352_특정거리도시찾기.java
@@ -0,0 +1,17 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_18352_특정거리도시찾기 {
+    public static int N,M,K,X;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
