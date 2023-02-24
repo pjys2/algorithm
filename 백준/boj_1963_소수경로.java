c5c1778b6b48690682a06d4757c7824763459809
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-24 17:25:07 +0900

    update

---
+++ 백준/boj_1963_소수경로.java
@@ -0,0 +1,18 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_1963_소수경로 {
+    public static int T;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T;tc++){
+
+        }
+
+    }
+}
