a4fcf5fab84cc08d4da01e3507280a7421e4d6b2
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-02-24 21:08:22 +0900

    update

--- 백준/boj_25307_시루의백화점구경.java
+++ 백준/boj_25307_시루의백화점구경.java
@@ -1,5 +1,6 @@
 package 백준;
 
+
 import java.io.*;
 import java.util.*;
 
@@ -8,6 +9,9 @@ public class boj_25307_시루의백화점구경 {
     static int[][] map;
     static int[] dr = {1,-1,0,0};
     static int[] dc = {0,0,1,-1};
+    public static List<Point> maneList;
+    public static Point siruStart;
+    public static boolean[][] maneVisit;
     public static class Point{
         int r, c;
 
@@ -34,105 +38,91 @@ public class boj_25307_시루의백화점구경 {
         K = Integer.parseInt(st.nextToken());
         map = new int[N][M];
 
-        Point siru = new Point();
-        for(int i =0; i< N; i++){
+        maneList = new ArrayList<>();
+
+        maneVisit = new boolean[N][M];
+
+        for (int r = 0; r<N;r++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<M;j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 4){
-                    siru.r = i;
-                    siru.c = j;
+            for (int c = 0; c<M;c++){
+                map[r][c] = Integer.parseInt(st.nextToken());
+                if(map[r][c] == 3){
+                    maneList.add(new Point(r,c));
+                }else if(map[r][c] == 4){
+                    siruStart = new Point(r,c);
                 }
             }
         }
 
-        boolean[][] visited = new boolean[N][M];
-        for(int i =0; i< N; i++){
-            for(int j = 0; j<M;j++){
-                if(map[i][j] == 3){
-                    maneFind(new Point(i,j),visited);
-                }
-            }
+        for (Point mane : maneList){
+            mapCheck(mane);
         }
 
-
-
-        int ans = move(siru);
-        System.out.println(ans);
-
+        siruMove(siruStart);
 
     }
 
-    private static void maneFind(Point mane, boolean[][] visited) {
-        Queue<Point> queue = new LinkedList<Point>();
-        queue.offer(mane);
-        map[mane.r][mane.c] = 1;
-        visited[mane.r][mane.c] = true;
-        int distance = -1;
-        while(!queue.isEmpty()){
-            distance++;
-            if(distance == K) break;
-            int length = queue.size();
-//            System.out.println("------------------------------------------");
-            for(int i =0;i<length;i++){
+    public static void mapCheck(Point start){
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(start);
+        maneVisit[start.r][start.c] = true;
+
+        for (int cnt = 0;cnt<=K;cnt++){
+            int size = queue.size();
+            for(int s = 0; s<size;s++) {
                 Point current = queue.poll();
-//                System.out.println("좌표 : ["+current.r+", "+current.c+"]");
+
+                map[current.r][current.c] = -1;
 
 
-                for (int d = 0; d<4;d++) {
+                for (int d = 0; d<4;d++){
+                    int nr = current.r+dr[d];
+                    int nc = current.c+dc[d];
+                    if(nr >= 0 && nr < N && nc >= 0 && nc < M && !maneVisit[nr][nc]){
+                        queue.add(new Point(nr,nc));
+                        maneVisit[nr][nc] = true;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static void siruMove(Point start){
+        Queue<Point> queue = new LinkedList<>();
+
+        queue.add(start);
+        boolean[][] visited = new boolean[N][M];
+        visited[start.r][start.c] = true;
+
+        int cnt = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for (int s = 0; s<size;s++){
+                Point current = queue.poll();
+
+                if(map[current.r][current.c] == 2){
+                    System.out.println(cnt);
+                    return;
+                }
+
+                for (int d = 0; d<4;d++){
                     int nr = current.r + dr[d];
                     int nc = current.c + dc[d];
-
-//                        System.out.println("nr : "+nr+", nc :"+nc);
-
-
-                    if (nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc]) {
-                        if(map[nr][nc] != 3) {
-                            map[nr][nc] = 1;
-                        }
-                        queue.offer(new Point(nr, nc));
+                    if(nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && map[nr][nc] != -1){
+                        queue.add(new Point(nr,nc));
                         visited[nr][nc] = true;
                     }
                 }
             }
 
+            cnt++;
         }
-//        System.out.println("["+mane.r+", "+mane.c+"]");
-//        print(map);
-//        System.out.println("----------------------------");
+
+        System.out.println(-1);
     }
 
-    private static int move(Point siru) {
-        Queue<Point> queue = new LinkedList<Point>();
-        queue.offer(siru);
-        boolean[][] visited = new boolean[N][M];
-        visited[siru.r][siru.c] = true;
-
-        int energy = 0;
-        while(!queue.isEmpty()){
-            energy++;
-            int length = queue.size();
-            for(int i = 0; i<length;i++){
-                Point current = queue.poll();
-
-
-                for(int d = 0; d<4;d++){
-                    int nr = current.r +dr[d];
-                    int nc = current.c +dc[d];
-                    if(nr >=0 && nr <N && nc >=0 && nc <M && !visited[nr][nc] && map[nr][nc] != 1){
-                        if(map[nr][nc] == 2){
-                            return energy;
-                        }
-
-                        queue.offer(new Point(nr,nc));
-                        visited[nr][nc] = true;
-
-                    }
-                }
-            }
-        }
-        return -1;
-    }
 
     private static void print(int[][] map) {
         for(int i =0; i< N; i++){
