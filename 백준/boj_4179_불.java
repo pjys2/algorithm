d297ab78934e466d2709723cc6f1e6afcffe15a4
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-03-01 23:21:16 +0900

    update

---
+++ 백준/boj_4179_불.java
@@ -0,0 +1,24 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_4179_불 {
+    public static int R,C;
+    public static char[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new char[R][C];
+
+
+
+
+
+    }
+}
