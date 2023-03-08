769c966fe47c3381448f03375438504705e22016
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-03-09 01:51:07 +0900

    update

--- 백준/boj_14442_벽부수고이동하기2.java
+++ 백준/boj_14442_벽부수고이동하기2.java
@@ -1,8 +1,21 @@
 package 백준;
 
-public class boj_14442_벽부수고이동하기2 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    public static void main(String[] args) {
+public class boj_14442_벽부수고이동하기2 {
+    public static int N, M;
+    public static int[][] map;
+    public static int[] dr = {0,0,1,-1};
+    public static int[] dc = {1,-1,0,0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         
     }
 }
