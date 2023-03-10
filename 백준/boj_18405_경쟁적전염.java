ff60324689ee676a0f35b0943a54fbe8507d9280
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-03-10 10:34:20 +0900

    update

---
+++ 백준/boj_18405_경쟁적전염.java
@@ -0,0 +1,31 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_18405_경쟁적전염 {
+    public static int N, K, S, X, Y;
+    public static int[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N+1][N+1];
+
+        for (int r = 1;r<=N;r++){
+            st = new StringTokenizer(br.readLine());
+            for (int c = 1; c<=N;c++){
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        S = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
+
+    }
+}
+
