d4aff0c2b40506d145d7f0082bbddd85e1e7aacb
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-04-01 21:25:55 +0900

    update

---
+++ 백준/boj_1039_교환.java
@@ -0,0 +1,104 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj_1039_교환 {
+    public static int N, K,ans,len;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        ans = 0;
+        int current = N;
+        if(current/1000000 != 0){
+            len = 7;
+        }else if(current/100000 != 0){
+            len = 6;
+        }else if(current/10000 != 0){
+            len = 5;
+        }else if(current/1000 != 0){
+            len = 4;
+        }else if(current/100 != 0){
+            len = 3;
+        }else if(current/10 != 0){
+            len = 2;
+        }else{
+            len = 1;
+        }
+
+        BFS();
+
+    }
+
+    public static void BFS(){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+        Set<Integer> visitSet = new HashSet<>();
+        visitSet.add(N);
+        int cnt = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for (int s = 0; s<size;s++){
+                int current = queue.poll();
+
+                if(cnt == K){
+                    ans = Math.max(ans, current);
+                }
+
+
+                int[] numbers = new int[len];
+
+                int idx = (int)Math.pow(10,len-1);
+                for (int k = 0; k<len;k++){
+                    numbers[k] = current/idx;
+                    current = current%idx;
+                    idx = idx / 10;
+                }
+
+                for (int i = 0; i<len-1;i++){
+                    for (int j = i;j<len;j++){
+
+                        if(i == j) continue;
+                        int next = 0;
+                        boolean check = true;
+                        idx = 1;
+                        for (int k = len-1; k>=0;k--){
+                            if(k==i){
+                                if(i == 0 && numbers[j] == 0) check = false;
+                                next += numbers[j]*idx;
+                            }else if (k == j){
+                                if(j == 0 && numbers[i] == 0) check = false;
+                                next += numbers[i]*idx;
+                            }else{
+                                if(k == 0 && numbers[k] == 0) check = false;
+                                next += numbers[k]*idx;
+                            }
+                            idx = idx * 10;
+                        }
+
+                        if (visitSet.contains(next)) continue;
+
+                        if(check){
+                            queue.add(next);
+                        }
+                    }
+                }
+            }
+
+            if(cnt == K) break;
+            cnt++;
+        }
+
+
+        if(cnt == K){
+            System.out.println(ans);
+        }else{
+            System.out.println(-1);
+        }
+    }
+}
