ccdb6649dd0a86ce3e791148128ded288831afe5
박재영 <fly_98@naver.com>
2023-04-29 19:51:10 +0900

    update

--- 백준/boj_13424_비밀모임.java
+++ 백준/boj_13424_비밀모임.java
@@ -3,18 +3,22 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class boj_13424_비밀모임 {
-    public static int T, N, M;
+    public static int T, N, M,K,ans,ansLen;
+    public static int[] room;
     public static List<Node>[] nodeList;
-    public static class Node{
+    public static class Node implements Comparable<Node>{
         int num, len;
         public Node(int num, int len){
             this.num = num;
             this.len = len;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.len - o.len;
         }
     }
 
@@ -27,7 +31,8 @@ public class boj_13424_비밀모임 {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
-
+            ansLen = Integer.MAX_VALUE;
+            ans = 0;
             nodeList = new List[N+1];
 
             for (int i = 1;i<=N;i++){
@@ -43,8 +48,58 @@ public class boj_13424_비밀모임 {
                 nodeList[b].add(new Node(a,c));
             }
 
+            K = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
 
-            
+            room = new int[K+1];
+
+            for (int i =1; i<=K;i++){
+                int roomNum = Integer.parseInt(st.nextToken());
+                room[i] = roomNum;
+            }
+
+            for (int i = 1; i<=N;i++){
+                dijkstra(i);
+            }
+
+            System.out.println(ans);
+        }
+    }
+
+    public static void dijkstra(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start,0));
+
+
+//        int bitMask = 0;
+        int[] distance = new int[N+1];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+
+        while(!pq.isEmpty()){
+            Node current = pq.poll();
+
+
+            if (distance[current.num] < current.len) continue;
+
+            for(Node next : nodeList[current.num]){
+                if (distance[next.num] >= current.len + next.len){
+                    pq.add(new Node(next.num,current.len + next.len));
+                    distance[next.num] = current.len + next.len;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 1; i<=K;i++){
+            sum += distance[room[i]];
+        }
+
+        if (ansLen > sum){
+            ansLen = sum;
+            ans = start;
+        }else if(ansLen == sum && ans > start){
+            ansLen = sum;
+            ans = start;
         }
     }
 }
