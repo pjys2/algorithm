ce5b7fc8d5cffb28419fdebee67baa6c19e43c35
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-05-09 23:12:10 +0900

    boj_1043 update

--- 백준/boj_1043_거짓말.java
+++ 백준/boj_1043_거짓말.java
@@ -1,16 +1,50 @@
 package 백준;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class boj_1043_거짓말 {
     public static int N, M;
     public static boolean[] people;
-    public static List<Integer>[] partyList;
+    public static int[] parrents;
+    public static void makeSet(int n){
+        for (int i = 1 ;i<=n;i++){
+            parrents[i] = i;
+        }
+    }
+
+    public static int find(int a){
+        if (parrents[a] == a) return a;
+
+        int ret = find(parrents[a]);
+
+        if (people[a]){
+            people[parrents[a]] = true;
+        }
+
+        if (people[parrents[a]]){
+            people[a] = true;
+        }
+
+        return ret;
+    }
+
+    public static void union(int a, int b){
+
+        int parrentB = find(b);
+
+        if (people[a]) people[b] = true;
+        if (people[b]) people[a] = true;
+
+        parrents[parrentB] = a;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -19,8 +53,8 @@ public class boj_1043_거짓말 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        parrents = new int[N+1];
         people = new boolean[N+1];
-
 
         st = new StringTokenizer(br.readLine());
 
@@ -28,13 +62,17 @@ public class boj_1043_거짓말 {
 
         int pNum = Integer.parseInt(st.nextToken());
 
+
+        makeSet(N);
+
         for (int i = 0; i<pNum;i++){
             int num = Integer.parseInt(st.nextToken());
             people[num] = true;
         }
 
 
-        partyList = new List[M+1];
+
+        List<Integer>[] partyList = new List[M+1];
 
         for (int i = 1; i<=M;i++){
             partyList[i] = new ArrayList<>();
@@ -42,36 +80,35 @@ public class boj_1043_거짓말 {
 
             int num = Integer.parseInt(st.nextToken());
 
-            boolean isTrue = true;
-            for (int j = 1; j<=num;j++){
-                int p =  Integer.parseInt(st.nextToken());
-                partyList[i].add(p);
-                if (people[p]){
-                    isTrue = false;
-                }
-            }
+            int a = Integer.parseInt(st.nextToken());
+            partyList[i].add(a);
+            for (int j = 1;j<num;j++){
+                int b = Integer.parseInt(st.nextToken());
+                partyList[i].add(b);
+                union(a,b);
 
-            if (!isTrue){
-                for (int p : partyList[i]){
-                    people[p] = true;
-                }
+                a = b;
             }
-
         }
 
         int ans = 0;
+
+        System.out.println(Arrays.toString(parrents));
+        System.out.println(Arrays.toString(people));
         for (int i = 1; i<=M;i++){
             boolean isPossible = true;
             for (int p : partyList[i]){
-                if (people[p]){
+                if (people[p]) {
                     isPossible = false;
                     break;
                 }
             }
 
-            if (isPossible) ans++;
-
+            if (isPossible){
+                ans++;
+            }
         }
+
 
 
         System.out.println(ans);
