95f47af4e086e70ad9f8e536a0280b8126eb99ca
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2023-06-11 22:01:35 +0900

    boj_2617 update

---
+++ 백준/boj_2617_구슬찾기.java
@@ -0,0 +1,22 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_2617_구슬찾기 {
+    public static int N, M;
+    public static int[] marble;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        marble = new int[N+1];
+
+
+    }
+}
